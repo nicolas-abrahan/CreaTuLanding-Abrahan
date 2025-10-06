a43e38b2e834329973d8621c166b23fee87ca25a
nicolas-abrahan-dev <nicolasmartinabrahan@gmail.com>
2025-10-06 19:32:56 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 
-
 function App() {
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting = "Bienvenidos a Crucial Sin" />
+      <ItemListContainer greeting="Bienvenidos Mi ecommerce" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,43 @@
 import CartWidget from "./CartWidget";
 import Logo from "../assets/logo.webp";
 import style from "./NavBar.module.css";
-import '@fontsource/poppins/400.css'; 
-import '@fontsource/poppins/700.css'; 
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
 
 const NavBar = () => {
-    return (
-        <nav className={style.navBar}>
-            <img className={style.logo} src={Logo} alt="Logo Crucial Sin" />
-            <ul className={style.navLinks}>
-                <li><a className={style.links} href="#">Inicio</a></li>
-                <li><a className={style.links} href="#">Productos</a></li>
-                <li><a className={style.links} href="#">Contacto</a></li>
-            </ul>
-            <CartWidget />
-        </nav>
-    )
-}
+  return (
+    <nav className={style.navBar}>
+      <img className={style.logo} src={Logo} alt="Logo Crucial Sin" />
+      <ul className={style.navLinks}>
+        <li>
+          <a className={style.links} href="#">
+            Inicio
+          </a>
+        </li>
+        <li>
+          <a className={style.links} href="#">
+            Hombres
+          </a>
+        </li>
+        <li>
+          <a className={style.links} href="#">
+            Mujeres
+          </a>
+        </li>
+        <li>
+          <a className={style.links} href="#">
+            Niños
+          </a>
+        </li>
+        <li>
+          <a className={style.links} href="#">
+            Contacto
+          </a>
+        </li>
+      </ul>
+      <CartWidget />
+    </nav>
+  );
+};
 
 export default NavBar;
